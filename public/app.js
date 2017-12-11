55b3cebe2993a6cecf88e3dfbbaebd7b0dcd9bc6
Kynan <k.song@blueyonder.co.uk>
2017-12-11 20:36:51 +0000

    Cats now in array.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
-// const cats = [
-//   {"name": "fsaewvav", "Favourite food": "agvdsa", "Image":"http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"},
-//   {"name": "Marta", "food": "Cheese", "image":"https://www.pets4homes.co.uk/images/articles/3658/large/the-basics-of-caring-for-a-tortoise-57c06846bce6c.jpg"}
-// ];
+const cats = [
+  {"name": "fsaewvav", "food": "agvdsa", "image":"https://cdn.shopify.com/s/files/1/0344/6469/products/New-Cute-Pet-Cat-Costume-Cartoon-Funny-Pet-Cat-Clothes-Hot-Puppy-Clothing-Doctor-Uniform-Hat_copy_1024x1024.jpg?v=1504800100"},
+  {"name": "Marta", "food": "Cheese", "image":"https://www.pets4homes.co.uk/images/articles/3658/large/the-basics-of-caring-for-a-tortoise-57c06846bce6c.jpg"},
+  {"name": "Bob", "food": "Cheese", "image":"http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"},
+  {"name": "Boba", "food": "Sock fluff", "image":"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {"name": "Barnaby", "food": "Tuna", "image":"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {"name": "Max", "food": "Whiskas Temptations", "image":"http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
+];
+
 
 var app = function() {
   console.log("vsveres");
@@ -30,7 +35,11 @@ var app = function() {
   // const cats = document.querySelector('#cats');
   // cats.appendChild(createCat);
 
-  createCat("bob", "cheese", "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg");
+  for(let cat of cats) {
+    createCat(cat.name, cat.food, cat.image);
+  }
+
+  // createCat("bob", "cheese", "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg");
 
 };
 
